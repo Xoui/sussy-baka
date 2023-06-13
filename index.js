a3f471c33538db7d57384b64cc47a4e40078818a
Xoui <136085758+Xoui@users.noreply.github.com>
2023-06-13 20:57:24 +0000

    update files

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-(async() => {
-    await import('./index.mjs');
-  })();
-  
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3000, function () {
+  console.log('App is listening on port 3000!');
+});
